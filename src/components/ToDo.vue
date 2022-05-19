3c05f7460b7563787f7968af685b168a30864241
Panoskontos <panos96el@gmail.com>
2022-05-19 16:56:35 +0300

    first commit

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,5 +1,22 @@
 <template>
     <div>
-        {{$store.state.firstName}} {{$store.state.lastName}}
+        <div>
+            States
+            <p>
+                        {{$store.state.firstName}} -  {{$store.state.lastName}} - {{$store.state.favorites}}
+            </p>
+        </div>
+        <br>
+        <br>
+        <div>
+            Getter Function
+            <p>
+                {{$store.getters.fullName}}
+            </p>
+        </div>
+
+
+        <Button @click="$store.dispatch('addToFavorites', 'element')">Add to favorites</Button>
+
     </div>
 </template>
